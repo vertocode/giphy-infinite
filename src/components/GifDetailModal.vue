ac1803b079d0b95ece4908ee6a267fad770f95af
vertocode <evertonvanoni1@gmail.com>
2024-02-08 21:24:40 -0300

    feat: add download for the gif after opening the details

---
+++ src/components/GifDetailModal.vue
@@ -0,0 +1,76 @@
+<template>
+  <q-dialog :model-value="show" @hide="show = false">
+    <q-card>
+      <q-card-section class="bg-secondary text-center">
+        <q-icon
+          name="close"
+          class="text-2xl absolute top-2 right-2 cursor-pointer text-white"
+          @click="show = false"
+        />
+        <h3 class="text-slate-50 text-2xl text-bold text-wrap mt-6">{{ gif.title }}</h3>
+        <div class="column items-center">
+          <q-img class="w-52 sm:w-96 mt-3 mx-auto" :src="gif.images.original.url" />
+          <q-btn
+            class="bg-slate-200 w-52 sm:w-96"
+            label="Download"
+            @click="download"
+          />
+        </div>
+      </q-card-section>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+const show = ref(false)
+
+interface Props {
+  gif: object | null
+}
+
+const props = defineProps<Props>()
+const emits = defineEmits(['close'])
+
+const download = () => {
+  const gifUrl = props.gif?.images?.original?.url;
+
+  if (gifUrl) {
+    // Fetch the GIF content as a Blob
+    fetch(gifUrl)
+      .then(response => response.blob())
+      .then(blob => {
+        // Create a Blob URL for the response
+        const blobUrl = URL.createObjectURL(blob);
+
+        // Create a temporary link
+        const link = document.createElement('a');
+        link.href = blobUrl;
+        link.download = props.gif.title.replaceAll(' ', '_').toLowerCase() + '.gif'
+
+        // Add the link to the DOM and trigger a click
+        document.body.appendChild(link);
+        link.click();
+
+        // Remove the link and revoke the Blob URL
+        document.body.removeChild(link);
+        URL.revokeObjectURL(blobUrl);
+      })
+      .catch(error => {
+        console.error('Error downloading:', error);
+      });
+  }
+};
+
+watch(() => show.value,() => {
+  if (!show.value) {
+    emits('close')
+  }
+})
+
+watch(() => props.gif, () => {
+  if (props.gif) {
+    show.value = true
+  }
+})
+</script>
--- src/components/GifList.vue
+++ src/components/GifList.vue
@@ -1,6 +1,11 @@
 <template>
+  <GifDetailModal :gif="gitDetailsModal" @close="gitDetailsModal = null"/>
   <main class="mt-8 flex gap-2 w-full px-10 justify-between items-center">
-    <div v-for="gif in gifs" :key="gif.id" class="bg-secondary cursor-pointer">
+    <div
+      v-for="gif in gifs" :key="gif.id"
+      class="bg-secondary cursor-pointer"
+      @click="gitDetailsModal = gif"
+    >
       <q-img :src="gif.images.original.url" class="w-12 w-52" />
     </div>
   </main>
@@ -11,11 +16,14 @@
 
 <script setup lang="ts">
 import { useGiphyFetch } from 'src/composables/useGiphyFetch'
-import { onMounted, computed, onBeforeUnmount } from 'vue'
+import { onMounted, computed, onBeforeUnmount, ref } from 'vue'
 import { useGiphyStore } from 'src/stores/giphyStore'
+import GifDetailModal from 'components/GifDetailModal.vue'
 
 const { getGifs } = useGiphyFetch()
 const giphyStore = useGiphyStore()
+
+const gitDetailsModal = ref(null)
 
 const gifs = computed(() => giphyStore.gifs)
 const isLoading = computed(() => giphyStore.isLoading)
@@ -25,8 +33,8 @@ const handleScroll = async () => {
   const windowHeight = window.innerHeight;
   const bodyHeight = document.body.offsetHeight;
 
-  // Check if the scroll is almost in the end (90% of the page) and if is not in loading to add more items in the list
-  if ((scrollY + windowHeight >= bodyHeight * 0.9) && !isLoading.value) {
+  // Check if the scroll is almost in the end (95% of the page) and if is not in loading to add more items in the list
+  if ((scrollY + windowHeight >= bodyHeight * 0.95) && !isLoading.value) {
     await getGifs()
   }
 }
