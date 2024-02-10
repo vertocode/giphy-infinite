a58affde6470a9bf019734a76dac2ab546c2fbe8
vertocode <evertonvanoni1@gmail.com>
2024-02-10 08:06:54 -0300

    fix: unnecessary comments

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,9 +1,5 @@
-/* eslint-disable */
-// https://github.com/michael-ciniawsky/postcss-load-config
-
 module.exports = {
   plugins: [
-    // https://github.com/postcss/autoprefixer
     require('autoprefixer')({
       overrideBrowserslist: [
         'last 4 Chrome versions',
@@ -17,12 +13,5 @@ module.exports = {
       ]
     }),
     require('tailwindcss')
-
-    // https://github.com/elchininet/postcss-rtlcss
-    // If you want to support RTL css, then
-    // 1. yarn/npm install postcss-rtlcss
-    // 2. optionally set quasar.config.js > framework > lang to an RTL language
-    // 3. uncomment the following line:
-    // require('postcss-rtlcss')
   ]
 }
